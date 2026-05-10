b6467f9a030bc96ed3ce4080e7475be2c59fec05
stevenstinnett <stevenstinnett@comcast.net>
2026-05-10 11:21:55 -0400

    Update upload-handler.js
    Add multipart/form-data parsing to upload-handler.js

--- netlify/functions/upload-handler.js
+++ netlify/functions/upload-handler.js
@@ -1,3 +1,5 @@
+const busboy = require("busboy");
+
 exports.handler = async (event, context) => {
   if (event.httpMethod !== "POST") {
     return {
@@ -6,10 +8,44 @@ exports.handler = async (event, context) => {
     };
   }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "upload-handler.js received your POST request!"
-    })
-  };
+  return new Promise((resolve, reject) => {
+    const bb = busboy({ headers: event.headers });
+    const files = [];
+    const fields = {};
+
+    bb.on("file", (name, file, info) => {
+      const { filename, mimeType } = info;
+      let fileBuffer = [];
+
+      file.on("data", data => {
+        fileBuffer.push(data);
+      });
+
+      file.on("end", () => {
+        files.push({
+          fieldName: name,
+          filename,
+          mimeType,
+          size: Buffer.concat(fileBuffer).length
+        });
+      });
+    });
+
+    bb.on("field", (name, value) => {
+      fields[name] = value;
+    });
+
+    bb.on("finish", () => {
+      resolve({
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "upload-handler.js received your file(s)!",
+          fields,
+          files
+        })
+      });
+    });
+
+    bb.end(Buffer.from(event.body, "base64"));
+  });
 };
